936456ea982b3ce2d46c2a61665a8719c6da9ac0
Yuyang Xia <3104964206@qq.com>
2025-03-13 11:14:34 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,66 +1,62 @@
 #include "game.h"
-
 #include <iostream>
+#include <vector>
 using namespace std;
-int p[9] = {2,3,5,7,11,13,17,19,23};
-char board[11] = {'\0','\0','\0','\n','\0','\0','\0','\n','\0','\0','\0'};
 
-int *devide(long long n){
-  int power[9]={};
-  for(int i=0;i<9;i++){
-    while (n%p[i]==0){
-      power[i]+=1;
+const int p[9] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+char board[11] = {'-', '-', '-', '\n', '-', '-', '-', '\n', '-', '-', '-'};
+
+vector<int> devide(long long n) {
+  vector<int> power(9, 0);
+  for (int i = 0; i < 9; i++) {
+    while (n % p[i] == 0) {
+      power[i]++;
+      n /= p[i];
     }
-   }
+  }
   return power;
 }
 
 long long play(long long code, int player, int x, int y) {
-  long long result = code;
-  int rank = (x-1)*3+y-1;
-  result += p[rank]^player;
-  return result;
+  int rank = (x-1)*3+(y-1);
+  return code + (p[rank] ^ player);
 }
 
 void print(long long code) {
-  //int p[9] = {2,3,5,7,11,13,17,19,23};
-  //char board[11] = {'\0','\0','\0','\n','\0','\0','\0','\n','\0','\0','\0'};
-  int *power = devide(code);
+  vector<int> power = devide(code);
   for (int i = 0; i < 9; i++) {
     if (power[i] == 1)
-      board[i] ='x';
-    if (power[i] == 2)
+      board[i] = 'x';
+    else if (power[i] == 2)
       board[i] = 'o';
-    if (power[i] == 0)
+    else
       board[i] = '-';
   }
-  for (int i = 0; i < 9; i++) {
-    cout << board[i];
+  for (int i = 0; i < 11; i++)
+  {
+    if (i % 4 != 3)
+      cout << board[i] << " ";
+    else
+      cout << board[i];
   }
-  cout << endl;
 }
 
 int check_winner(long long code) {
   for (int i = 0; i < 3; i++) {
-    if (board[i] == board[i+3] == board[i+6]) {
-      if (board[i] == 'x')
-        return 1;
-      if (board[i] == 'o')
-        return 2;
-        }
-       }
-  int j=0;
-  while (j<9) {
-    if (board[j] == board[j+1] == board[j+2]){
-      if (board[j] == 'x')
-        return 1;
-      if (board[j] == 'o')
-        return 2;
-      }
-    j += 3;
-   }
-  if ((board[0] == board[4] == board[8] == 'x') || (board[2] == board[4] == board[6] == 'x'))
-    return 1;
-  if ((board[0] == board[4] == board[8] == 'o') || (board[2] == board[4] == board[6] == 'o'))
-    return 2;
+    if (board[i] != '0' && board[i] == board[i + 3] && board[i] == board[i + 6]) {
+      return (board[i] == 'x') ? 1 : 2;
+    }
+  }
+  for (int i = 0; i < 9; i += 3) {
+    if (board[i] != '-' && board[i] == board[i + 1] && board[i] == board[i + 2]) {
+      return (board[i] == 'x') ? 1 : 2;
+    }
+  }
+  if (board[0] != '-' && board[0] == board[4] && board[0] == board[8]) {
+    return (board[0] == 'x') ? 1 : 2;
+  } else if (board[2] != '-' && board[2] == board[4] && board[2] == board[6]) {
+    return (board[2] == 'x') ? 1 : 2;
+  }
+  return 0;
 }
+
